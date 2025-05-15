da3d41dd0c54da87c5960503f473f1436b8b7e9c
soupmax <max.gottschaldt@gmail.com>
2025-05-15 10:23:07 +0200

    pyramiden, converter, dreiceck

---
+++ OOE/ueb/Klassen/Triangle.java
@@ -0,0 +1,29 @@
+package Klassen;
+
+//aufgabe 2
+public class Triangle {
+    private Point p1;
+    private Point p2;
+    private Point p3;
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public double getPerimeter() {
+        double a = p1.distance(p2);
+        double b = p2.distance(p3);
+        double c = p3.distance(p1);
+        return a + b + c;
+    }
+
+    public double getArea() {
+        double a = p1.distance(p2);
+        double b = p2.distance(p3);
+        double c = p3.distance(p1);
+        double s = (a + b + c) / 2.0;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
